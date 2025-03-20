4a5e3871d3ffcd9078311cf143327d70226ab79b
Again-D <nemcn5306@gmail.com>
2025-03-20 15:46:53 +0900

    Add Cam model with fields for id, name, url, created_at, and updated_at

--- apps/cam/models.py
+++ apps/cam/models.py
@@ -1,1 +1,10 @@
 from apps.app import db
+
+class Cam(db.Model):
+    __tablename__ = "cam"
+    id = db.Column(db.Integer, primary_key=True)
+    name = db.Column(db.String(100), nullable=False)
+    url = db.Column(db.String(100), nullable=False)
+    created_at = db.Column(db.DateTime, default=datetime.now)
+    updated_at = db.Column(db.DateTime, default=datetime.now, onupdate=datetime.now)
+    
