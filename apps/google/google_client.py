0980d96cfcb5740a2f439965c765235111b62d27
nino3116 <gusn3116@gmail.com>
2025-04-14 17:53:17 +0900

    UI: 회원가입 페이지 레이아웃 수정 및 카메라 관리 페이지 버튼 스타일 개선 페이지네이션 CSS적용, 비디오 목록 페이지네이션 구현 날짜검색 기능 수정

--- apps/google/google_client.py
+++
@@ -1,3 +0,0 @@
-GOOGLE_CLIENT_ID = '866807302030-06stqta500bfamkljkf97imisjhmnud7.apps.googleusercontent.com'
-GOOGLE_CLIENT_SECRET = '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'
-GOOGLE_DISCOVERY_URL = 'https://accounts.google.com/.well-known/openid-configuration'
